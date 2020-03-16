fa80ec3dc7df168c5756e98dae1840877ef2df1f
José Guilherme <jose.guilherme.ti@gmail.com>
2020-03-16 09:42:57 -0300

    first commit

--- index.js
+++ index.js
@@ -4,19 +4,33 @@ const usuarios = [{
     id: 1,
     nome: 'João Silva',
     email: 'jsilva@gmail.com',
-    idade: 29
+    idade: 29,
+    perfil_id: 1
 },
 {
     id: 2,
     nome: 'Rafael Junior',
     email: 'rafajun@gmail.com',
-    idade: 31
+    idade: 31,
+    perfil_id: 2
 },
 {
     id: 3,
     nome: 'Daniela Smith',
     email: 'danismi@gmail.com',
-    idade: 24
+    idade: 24,
+    perfil_id: 1
+}
+]
+
+
+const perfis  =[{
+    id: 1,
+    nome: 'Comum',
+},
+{
+    id: 2,
+    nome: 'Administrador',
 }
 ]
 
@@ -31,6 +45,7 @@ const typeDefs = gql`
         idade:Int
         salario:Float
         vip: Boolean
+        perfil: Perfil
     }
 
     type Produto{
@@ -39,6 +54,11 @@ const typeDefs = gql`
         preco: Float!
         desconto: Float
         precoComDesconto: Float
+    }
+
+    type Perfil{
+        id: Int
+        nome: String!
     }
 
 
@@ -52,6 +72,9 @@ const typeDefs = gql`
         numerosMegaSena: [Int!]!
         usuarios: [Usuario]
         usuario(id: Int) : Usuario
+        perfis:[Perfil]
+        perfil(id: Int) : Perfil
+
     }
 `
 
@@ -61,6 +84,11 @@ const resolvers = {
         salario(usuario) {
             return usuario.salario_real
         },
+        perfil(usuario){
+            const selecionados = perfis
+                .filter(u => u.id === usuario.perfil_id)
+            return selecionados ? selecionados[0] : null
+        }
     },
 
     Produto: {
@@ -117,8 +145,17 @@ const resolvers = {
             const selecionados = usuarios
                 .filter(u => u.id === id)
             return selecionados ? selecionados[0] : null
-        }
+        },
 
+        perfis(){
+            return perfis
+        },
+
+        perfil(_,{ id }) {
+            const selecionados = perfis
+                .filter(p => p.id === id)
+            return selecionados ? selecionados[0] : null
+        }
     }
 }
 
